300624e9666aa81bad95731958470fd612235ef8
Olga Volosevich <olga.lucky.v@gmail.com>
2020-03-17 17:30:43 +0300

    refactor: add adding todos via ItemAddForm

--- src/components/ItemAddForm/index.jsx
+++ src/components/ItemAddForm/index.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import classes from './ItemAddForm.module.scss';
 
  class ItemAddForm extends Component {
+
+    state = {
+        label : ''
+    }
    
+    onInputChange = (e) => {
+        this.setState({
+            label : e.target.value
+        })
+    }
+
+    onAddItem = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({
+           label : ''
+        })
+    }
     render () {
         return (
-            <div>
-                <Button onClick={()=>this.props.addItem('Hello!')} color="primary">primary</Button>
-            </div>
+            <Form onSubmit={this.onAddItem}>
+                <FormGroup className={classes.newTodoForm}>
+                    <Input  className={classes.newTodoInput}
+                            type="text" 
+                            placeholder="What else you need to do?"
+                            onChange={this.onInputChange}
+                            value={this.state.label}/>
+                    <Button color="primary">Add Todo</Button>
+                </FormGroup>
+            </Form>
         )
     }
 }
