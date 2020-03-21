5702cc48f38079d001a30d26466a85738c49624a
Olga Volosevich <olga.lucky.v@gmail.com>
2020-03-21 16:48:57 +0300

    Updates

---
+++ precache-manifest.42a4086a9d50d33094581a251959eece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed660885eeaddb498e8df307fc3c430c",
+    "url": "/To-Do-List-React-App/index.html"
+  },
+  {
+    "revision": "9d5b5aa3bd4b58bddf7c",
+    "url": "/To-Do-List-React-App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d6f79138d8bf8e0c7a93",
+    "url": "/To-Do-List-React-App/static/css/main.db88e2b0.chunk.css"
+  },
+  {
+    "revision": "9d5b5aa3bd4b58bddf7c",
+    "url": "/To-Do-List-React-App/static/js/2.2cc64300.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/To-Do-List-React-App/static/js/2.2cc64300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f79138d8bf8e0c7a93",
+    "url": "/To-Do-List-React-App/static/js/main.4de82f6b.chunk.js"
+  },
+  {
+    "revision": "781bdde62c889050d294",
+    "url": "/To-Do-List-React-App/static/js/runtime-main.951bcd47.js"
+  }
+]);
